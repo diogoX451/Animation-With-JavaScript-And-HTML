8943a829264c4caab0b1ce25b53e349e9590ed64
Diogo Almeida <diogo.g07@icloud.com>
2022-05-21 12:48:45 -0300

    master

--- src/animation.js
+++ src/animation.js
@@ -1,6 +1,10 @@
+//Função para fazer animação e delete das letras
 function typeWrite(elemento){
+    //Dividir em cada letra
     const text = elemento.innerHTML.split('');
+    //Apagar inicialmente 
     elemento.innerHTML = '';
+    //condição para aparecer letra por letra
     text.forEach((letra, i) =>{
         setTimeout(() => {
             elemento.innerHTML += letra;
@@ -8,8 +12,6 @@ function typeWrite(elemento){
     });
     
 }
-
-
-
+//Seleciona a tag do HTML
 const title = document.querySelector('p');
 typeWrite(title);
